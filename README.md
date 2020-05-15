07bfcce4548780d67ca4a17414ddd7db5d2ce225
Vortexxx <techsupport@riseup.net>
2020-05-15 00:44:06 -0700

    Added command example!

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#ArchAngel Selfbot
+# ArchAngel Selfbot
 A **Discord Selfbot** aimed for improving **customization and useability** of traditional selfbots while 
 adding **more functionality**.
 
---
+++ commands/example.py
@@ -0,0 +1,13 @@
+# Function name must be the same as the file name without '.py'
+# Make the parameters (bot, msg, prefix, cfg) in that order
+
+def example(bot, msg, prefix, cfg):
+    # Here you put what you would usually put in on_message().
+    # For example:
+    if msg.author == bot.user:
+        if msg.startswith(f"{prefix}hey"):
+            await msg.channel.send("> Hello!")
+        else:
+            return False
+    else:
+        return False
